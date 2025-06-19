e53d15847cea4c65d2df258464199a8ee97c59cb
Yash Rajurkar <yashrajurkar04@gmail.com>
2025-06-19 20:39:14 +0530

    Other Paths Upadted

--- function.js
+++ function.js
@@ -5,14 +5,14 @@ modeBtn.addEventListener("click", () => {
     if(cmode==="light"){
         cmode="dark";
    
-        document.getElementById("mode").style.backgroundImage='url(/Projects/PortfolioWebsite_Assignment/Images/Light.jpg)';
+        document.getElementById("mode").style.backgroundImage='url(/Images/Light.jpg)';
         document.querySelector("body").classList.add("dark");
         document.querySelector("body").classList.remove("light");
      
     }else{
         cmode = "light";
     
-        document.getElementById("mode").style.backgroundImage='url(/Projects/PortfolioWebsite_Assignment/Images/Moon.png)';
+        document.getElementById("mode").style.backgroundImage='url(/Images/Moon.png)';
         document.querySelector("body").classList.add("light");
         document.querySelector("body").classList.remove("dark");
     }
